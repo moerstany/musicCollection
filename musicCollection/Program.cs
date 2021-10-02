d76e11ec60b8f97f2120fa6f19305ab7821c96cc
Tanya <moerstany@gmail.com>
2021-10-02 22:23:13 +0700

    Initial commit

---
+++ musicCollection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace musicCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
